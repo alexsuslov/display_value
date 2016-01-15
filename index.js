cc1b5124415a8e3e454d4f7b6e941ed282883859
Alex Suslov <suslov.alex@gmail.com>
2016-01-15 09:13:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,31 +4,6 @@
 'use strict';
 define(['widgets'], function(d) {
   var debug = false;
-  d.directive('displayValue', function($compile) {
-    // Templates
-    var Template = [
-      '<svg width="{{width}}" height="{{height}}">',
-      '<g height="100%" ng-repeat="number in numbers" ',
-      'transform="translate({{ 70 * $index }}, 0) scale({{scale}})">',
-      // 0
-      '<path d="M 6.07 92.44 L 11.17 87.72 L 42.93 87.71 L 46.71 92.44 L 41.61 96.97 L 9.85 96.79 Z" fill="{{color[0]}}" />',
-      // 1
-      '<path d="M 54.64 50.28 L 58.41 54.82 L 54.06 86.01 L 48.77 90.54 L 44.99 86.20 L 49.33 54.82 Z" fill="{{color[1]}}" />',
-      // 2
-      '<path d="M 11.92 48.39 L 17.22 44.04 L 48.9 44.04 L 52.95 48.39 L 47.84 52.93 L 15.89 52.93 Z" fill="{{color[2]}}" />',
-      // 3
-      '<path d="M 10.227 50.28 L 14.00 54.82 L 9.660 86.0 L 4.36 90.54 L .5871 86.2 L 4.93 54.82 Z" fill="{{color[3]}}" />',
-      // 4
-      '<path d="M 60.79 6.427 L 64.5 10.964 L 60.22 42.15 L 54. 46.6 L 51.15 42.34 L 55.50 10.964 Z" fill="{{color[4]}}" />',
-      // 5
-      '<path d="M 18.35 4.53 L 23.4 0 L 55.21 0 L 58.99 4.53 L 53.89 9.2 L 22.32 9.2 Z" fill="{{color[5]}}" />',
-      // 6
-      '<path d="M 16.29 6.42 L 20.07 10.964 L 15.72 42.15 L 10.436 46.6 L 6.65 42.34 L 11.00 10.964 Z" fill="{{color[6]}}" />',
-      // dot
-      '<path d="M 61.01 84.97 L 61.01 84.97 C 64.3 84.97 67.01 87.66 67.01 90.97 L 67.01 90.97 C 67.01 94.28 64.3 96.97 61.01 96.97 L 61.01 96.97 C 57.69 96.97 55.01 94.28 55.01 90.97 L 55.01 90.97 C 55.01 87.66 57.69 84.97 61.01 84.97 Z" fill="{{color[7]}}" />',
-      '</g>',
-      '</svg>'
-    ]
     /*
       5
     6   4
@@ -69,6 +44,64 @@ define(['widgets'], function(d) {
         c & 0b10000000 ? on : off
       ]
     }
+
+  d.directive('ngNumber', function() {
+    return function(scope, el, attrs) {
+      var on = scope.$parent.onColor;
+      var off = scope.$parent.offColor;
+      el.attr('fill',  digital(scope.number, on, off)[attrs.ngNumber] );
+
+    }
+  })
+  d.directive('ngHeight', function() {
+    return function(scope, el, attrs) {
+      attrs.$observe('ngHeight', function(value) {
+        attrs.$set('height', value);
+      })
+    }
+  })
+
+  d.directive('ngTransform', function() {
+    return function(scope, el, attrs) {
+      el.attr( 'transform',  [
+        "translate(",
+        70 * Number(attrs.index) * Number(attrs.scale),
+        ", 0) scale(",
+        attrs.scale,
+        ")"
+        ].join(''));
+    };
+  })
+  d.directive('displayvalue', function() {
+    // Templates
+    var Template = [
+    // svg
+      '<svg ng-height="{{height}}">',
+      // group
+      '<g height="100%" ng-repeat="number in numbers" on="{{onColor}}"',
+      'ng-transform="{{$index}}" index="{{$index}}" scale="{{scale}}">',
+      // 0
+      '<path d="M 6.07 92.44 L 11.17 87.72 L 42.93 87.71 L 46.71 92.44 L 41.61 96.97 L 9.85 96.79 Z" ng-number="{{0}}" />',
+      // 1
+      '<path d="M 54.64 50.28 L 58.41 54.82 L 54.06 86.01 L 48.77 90.54 L 44.99 86.20 L 49.33 54.82 Z" ng-number="{{1}}" />',
+      // 2
+      '<path d="M 11.92 48.39 L 17.22 44.04 L 48.9 44.04 L 52.95 48.39 L 47.84 52.93 L 15.89 52.93 Z" ng-number="{{2}}" />',
+      // 3
+      '<path d="M 10.227 50.28 L 14.00 54.82 L 9.660 86.0 L 4.36 90.54 L .5871 86.2 L 4.93 54.82 Z" ng-number="{{3}}" />',
+      // 4
+      '<path d="M 60.79 6.427 L 64.5 10.964 L 60.22 42.15 L 54 46.6 L 51.15 42.34 L 55.50 10.964 Z" ng-number="{{4}}" />',
+      // 5
+      '<path d="M 18.35 4.53 L 23.4 0 L 55.21 0 L 58.99 4.53 L 53.89 9.2 L 22.32 9.2 Z" ng-number="{{5}}" />',
+      // 6
+      '<path d="M 16.29 6.42 L 20.07 10.964 L 15.72 42.15 L 10.436 46.6 L 6.65 42.34 L 11.00 10.964 Z" ng-number="{{6}}"" />',
+      // dot
+      '<path d="M 61.01 84.97 L 61.01 84.97 C 64.3 84.97 67.01 87.66 67.01 90.97 L 67.01 90.97 C 67.01 94.28 64.3 96.97 61.01 96.97 L 61.01 96.97 C 57.69 96.97 55.01 94.28 55.01 90.97 L 55.01 90.97 C 55.01 87.66 57.69 84.97 61.01 84.97 Z" ng-number="{{7}}" />',
+      '</g>',
+      '</svg>'
+    ].join('');
+    // console.log('template', Template);
+
+
     // add pointer to char
     // ["1",".", "3"] => ["1."+"3"]
     function pointer(arr){
@@ -119,34 +152,25 @@ define(['widgets'], function(d) {
       var widget = $scope.ngModel;
       // updater function
       function updater(){
-        // get status
-        var status = widget.get('value').status;
-        $scope.status = status != 0;
-        // color index
-        var idx = colors.indexOf( widget.get('color') );
-        // set color
-        // !important != 0
-        $scope.color = ( status != 0 && idx !== -1 ?  colors[idx] : 'grey');
-        // set description
-        $scope.descr = widget.get('descr');
+        console.log('--- updater');
+        scope.width = widget.get('width');
+        scope.height = widget.get('height')
         $scope.$apply();
       }
       // on
       widget.on( 'change', updater);
-
-      $scope.change = function(){
-        var broker = widget.get('broker');
-        if (debug) console.log('broker', broker);
-        broker.pub( widget.get('topic') + '/control', ($scope.status? '1': "0"), 1);
-      }
-
     }
 
     // ### link
     function link(scope, el, attr) {
+      scope.numbers = " ".split('');
       // widget model
-      var widget = $scope.ngModel;
-      var value;
+      var widget = scope.ngModel;
+      console.log('model',scope.ngModel.toJSON() );
+      // height scale
+      scope.height = widget.get('height') || 100;
+      var scale = scope.height / 100;
+
       // get value
       if (widget.get('value'))
         var value = widget.get('value').status;
@@ -157,33 +181,27 @@ define(['widgets'], function(d) {
       scope.height = widget.get('height') || 100;
       // scale
       scope.scale = scope.height / 100;
+      scope.numbers = "123".split('');
+      scope.onColor = widget.get('color');
+      scope.offColor = widget.get('inactive_color');
 
       // widget model
       var widget = scope.ngModel;
-      var h = model.height || 100;
+      var h = widget.height || 100;
       var l = pointer( value.split('') ).length;
       var width = 70 * h / 100 * pointer( value.split('')).length;
-
-
-
-
-      // // get status
-      // if (widget.get('value'))
-      //   var status = widget.get('value').status;
-      //   scope.status = status != 0;
-      // // color index
-      // var idx = colors.indexOf( widget.get('color') );
-      // // set color
-      // // !important != 0
-      // scope.color = ( status != 0 && idx !== -1 ?  colors[idx] : 'grey');
-      // // set description
-      // scope.descr = widget.get('descr');
+      scope.transform = function(index){
+        console.log('index', index);
+        return 'translate(' + 70 * index +', 0) scale(' + scale + ')'
+      }
     }
 
     return {
       restrict: 'E',
       priority : 10,
-      scope: {ngModel:"="},
+      scope: {
+        ngModel:"="
+      },
       template: Template,
       // controller: controller,
       link: link
